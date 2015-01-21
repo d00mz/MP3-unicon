2a7ce2a5d8603c5569e1727a7b4d2f4cc6166c4d
nightwaxx <ruvensprenger_macbook@pat1.sdi.h-da.de>
2015-01-21 12:20:19 +0100

    Geolocation Plugin installiert
    Geolocation Plugin installiert und Plugin für Accelerometer gelöscht

--- unicon/platforms/ios/www/cordova_plugins.js
+++ unicon/platforms/ios/www/cordova_plugins.js
@@ -1,24 +1,38 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/org.apache.cordova.device-motion/www/Acceleration.js",
-        "id": "org.apache.cordova.device-motion.Acceleration",
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
         "clobbers": [
-            "Acceleration"
+            "Coordinates"
         ]
     },
     {
-        "file": "plugins/org.apache.cordova.device-motion/www/accelerometer.js",
-        "id": "org.apache.cordova.device-motion.accelerometer",
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
         "clobbers": [
-            "navigator.accelerometer"
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
         ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.device-motion": "0.2.11"
+    "org.apache.cordova.geolocation": "0.3.11"
 }
 // BOTTOM OF METADATA
 });
