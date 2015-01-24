4a6607edc51559005fb921a897d0bdf111d6da63
d00mz <k.zwier@gmx.de>
2015-01-24 17:44:10 +0100

    Backup / Login
    Webaudio API Test in Backupordner verschoben
    AJAX Login vorbereitet

---
+++ MODX/js/login.js
@@ -0,0 +1,26 @@
+$.fn.serializeObject=function(){"use strict";var a={},b=function(b,c){var d=a[c.name];"undefined"!=typeof d&&d!==null?$.isArray(d)?d.push(c.value):a[c.name]=[d,c.value]:a[c.name]=c.value};return $.each(this.serializeArray(),b),a};
+
+
+
+$(document).ready(function(){
+
+	$('.loginLoginForm').submit(function(e) {
+		console.log($(this));
+		console.log($(this).serializeObject());
+
+		var form_data = $(this).serializeArray();
+		
+  		$.ajax({
+			type: "POST",
+			dataType: "html",
+            cache: false,
+			url: '/MP3/api/user/ajaxlogin/',
+			data: form_data,
+			success: function(data){
+				console.log($.parseJSON(data);
+			}
+		});
+		e.preventDefault();
+	});
+
+});
